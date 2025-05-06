c83c6a543498953ef7a1aac9846c6829a96bba6b
gigadatagit <gigadatagit@gmail.com>
2025-05-06 13:53:25 -0500

    Cambio en pagina de inicio

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import login as login
 
 archivo = __file__.split("/")[-1]
 login.generarLogin(archivo)
-if 'usuario' in st.session_state:
+if 'correo_electronico' in st.session_state:
     st.header('Página :orange[principal]')
     
     st.markdown("""
